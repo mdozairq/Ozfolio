6d097df5801415fa2d7b19b8d76e54d94f5a999b
mdozairq <mdozairq@gmail.com>
2021-08-26 23:24:24 +0530

    Created My Portfolio using react

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
         <div>
             <div className="col-md-4 col-10 mx-auto">
                                     <div class="card">
-                                        <img src={props.imgsrc} className="card-img-top" alt="" />
+                                        <img src={props.imgsrc} className="card-img-top" alt="{props.title}" />
                                         <div className="card-body">
                                             <h5 className="card-title font-weight-bold">{props.title}</h5>
                                             <p className="card-text">{props.description}</p>
